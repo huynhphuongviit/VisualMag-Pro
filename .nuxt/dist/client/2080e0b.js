bf2ad7ac24ac9bd551b1a6439a337dbd7f8fbad2
Huỳnh Văn Phương <141892553+huynhphuongviit@users.noreply.github.com>
2023-08-16 16:34:13 +0700

    add file dist

---
+++ .nuxt/dist/client/2080e0b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{450:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},o=t(23),component=Object(o.a)(l,(function(){return(0,this._self._c)("Layout")}),[],!1,null,null,null);e.default=component.exports}}]);
